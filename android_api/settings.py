8612a1fb7bd30718a146aa772e88adfb97624550
Amit Yadav <amityadav@amityadav.in>
2016-07-20 00:07:36 +0530

    fixed databases credentials in settings

--- android_api/settings.py
+++ android_api/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'android',
-        'USERNAME': 'root',
+        'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
